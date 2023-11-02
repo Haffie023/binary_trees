cba3853ab46ce24a70dae827f2b17c3d0ef07c69
Juli868 <bugijul@gmail.com>
2023-11-03 01:59:54 +0200

    Eighteenth task

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,3 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - finds a sibing to the given node.
+ * @node: node to work on
+ * Return: the sibling if any else NULL
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent;
+
+	if (node == NULL)
+		return NULL;
+	parent = node->parent;
+	if (parent == NULL)
+		return NULL;
+	if (parent->right == node)
+		return (parent->left);
+	if (parent->left == node)
+		return (parent->right);
+	return (NULL);
 }
