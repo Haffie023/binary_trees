3fc771ee9b8cbdb4a01d94926ac1719ab3a1027c
Juli868 <bugijul@gmail.com>
2023-11-03 02:04:25 +0200

    Eighteenth task

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,10 +9,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	binary_tree_t *parent;
 
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 	parent = node->parent;
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 	if (parent->right == node)
 		return (parent->left);
 	if (parent->left == node)
